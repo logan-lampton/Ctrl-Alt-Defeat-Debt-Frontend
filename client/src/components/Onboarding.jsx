12168114e09819b94e5eb985fd31c25e0f0aeaf1
Matthew <mthomaswicher@gmail.com>
2024-03-12 09:44:03 -0400

    add goal buttons to onboarding flow

--- client/src/components/Onboarding.jsx
+++ client/src/components/Onboarding.jsx
@@ -1,9 +1,20 @@
 import React from "react";
-import { Container } from "@mui/system";
+import { Container, ThemeProvider, createTheme } from "@mui/system";
+import pigbank from "../assets/pigbank.svg";
+import { Button } from "@mui/material";
 
 
 
 const Onboarding = () => {
+
+    const goals = [
+        "View all of my finances in one place",
+        "Track my income and spending",
+        "Save for a large purchase",
+        "Manage my money with a partner",
+        "Create a personalized budget",
+        "Save for a new home"
+    ];
 
 
 
@@ -14,14 +25,30 @@ const Onboarding = () => {
     return (
 
 <Container>
-<div>
-    <h1>
-        How would you like Money Magnet to assist you?
-    </h1>
-    <h3>
-        Tell us about your financial goals and Money Magnet will help you achieve them.
-    </h3>
+<div className="onboarding-header-container">
+    <header className="onboard-header">
+        <img className="headline-icon" src={pigbank} alt="piggybank-icon"></img>
+            <div>
+                <h1 className="headline-big">
+                How would you like Money Magnet to help you?
+                </h1>
+                <h3 className="gray-subtext">
+                Tell us about your financial goals and Money Magnet will help you achieve them.
+                </h3>
+            </div>
+    </header>
 </div>
+<section>
+    {goals.map((item) => {
+        return(
+<Button color="primary" className="goal-buttons" variant="contained">
+            {item}
+            </Button>
+            
+        )
+     
+    })}
+</section>
 
 
     </Container>
