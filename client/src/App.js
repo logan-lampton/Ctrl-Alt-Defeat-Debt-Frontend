af995ed5d0f3409e3a9074696c85e16bfc844a07
Matthew <mthomaswicher@gmail.com>
2024-03-13 12:30:43 -0400

    add navbar

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Onboarding from './components/Onboarding';
 import SignIn from './components/SignIn'
 import PhoneForm from './components/PhoneForm'
 import PhoneConfirm from './components/PhoneConfirm'
+import BottomNav from './components/BottomNav';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
               <PhoneForm />
               <SignIn />  */}
               <Onboarding/> 
+              <BottomNav/>
           </Container>
        
   );
