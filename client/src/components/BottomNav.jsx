24da9b86bc781626acddb6e0832257874ed1b731
Matthew <mthomaswicher@gmail.com>
2024-03-13 11:44:49 -0400

    add icons to bottom nav bar

--- client/src/components/BottomNav.jsx
+++ client/src/components/BottomNav.jsx
@@ -2,6 +2,14 @@ import { React, useState } from "react";
 import Container from '@mui/material/Container';
 import { BottomNavigation, BottomNavigationAction } from "@mui/material";
 import home from "../assets/home.svg";
+import goals from "../assets/goals.svg";
+import insights from "../assets/insights.svg";
+import accounts from "../assets/accounts.svg";
+
+
+
+
+
 const BottomNav = () => {
 
     const [selected, setSelected] = useState("Home");
@@ -10,14 +18,12 @@ const BottomNav = () => {
         setSelected(newValue);
       };
 
-
-
     return(
         <BottomNavigation onChange={{handleChange}}>
         <BottomNavigationAction label="Home" value="home" icon={<img src={home}/>} />
-        <BottomNavigationAction label="Goals" value="goals" icon={<img src={home}/>} />
-        <BottomNavigationAction label="Insights" value="insights" icon={<img src={home}/>} />
-        <BottomNavigationAction label="Accounts" value="accounts" icon={<img src={home}/>} />
+        <BottomNavigationAction label="Goals" value="goals" icon={<img src={goals}/>} />
+        <BottomNavigationAction label="Insights" value="insights" icon={<img src={insights}/>} />
+        <BottomNavigationAction label="Accounts" value="accounts" icon={<img src={accounts}/>} />
       </BottomNavigation>
     )
 }
