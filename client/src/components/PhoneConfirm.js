0734ee772be0a169280f36f840014ec2927b078a
njerimk <nmurugikamau@gmail.com>
2024-03-14 09:47:58 -0400

    nmk - resolved merged changes in App.js

--- client/src/components/PhoneConfirm.js
+++ client/src/components/PhoneConfirm.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import {Grid, Typography, TextField, Checkbox, styled, InputLabel} from '@mui/material'
+import {Link} from "react-router-dom"
+import ArrowBack from "../assets/arrow_back.svg"
 
 export default function PhoneConfirm(props) {
     
@@ -10,7 +12,8 @@ export default function PhoneConfirm(props) {
         color: '#333', // Customize label color
         display: 'flex',
         justifyContent: 'center', // Center horizontally
-        alignItems: 'center', // Center vertically
+        alignItems: 'center',
+        marginRight:'10px' // Center vertically
       }));
 
 
@@ -23,9 +26,14 @@ export default function PhoneConfirm(props) {
             alignItems="center"
             justifyContent="center"
             sx={{width: '425px'}}
+            columnSpacing={{xs:1}}
+        
 
         > 
-            
+            <Grid item xs={10}>
+             
+               <Link to='/phone-form'><img style={{}} src={ArrowBack} alt="ArrowBack" /></Link>
+            </Grid>
             <Grid item xs={12}>
                 <Typography align="center">
                     <h1 style={{fontStyle: 'TT Commons DemiBold', color:'black'}}>Confirm your number</h1>
@@ -33,7 +41,7 @@ export default function PhoneConfirm(props) {
             </Grid>
                 <Grid item xs={12}>
                     <Typography align="center">
-                        <p style={{ marginTop:'-10px',width:'95%',fontFamily:'TT Commons Regular', fontSize:'24px', color:'#718291', lineHeight:'22px'}}>Please submit the 6 digit confirmation code sent to 202-555-5555</p>
+                        <p style={{ marginTop:'-10px',width:'88%',fontFamily:'TT Commons Regular', fontSize:'22px', color:'#718291', lineHeight:'22px'}}>Please submit the 6 digit confirmation code sent to 202-555-5555</p>
                     </Typography>
                 </Grid> 
   
@@ -41,20 +49,18 @@ export default function PhoneConfirm(props) {
                     <div style={{ display: 'flex', justifyContent: 'center' }}>
                         <CenteredLabel htmlFor="confirmationCode" style={{fontFamily:'TT Commons Medium', color:'#DEE5EB', fontSize:'25px', marginTop:'20px'}}>Six Digit Code</CenteredLabel>
                     </div>
-                    <TextField fullWidth id="confirmationCode" variant="standard" style={{marginBottom:'20px',marginLeft: '10%',width: '75%'}} />
+                    <TextField fullWidth id="confirmationCode" variant="standard" style={{marginBottom:'20px',marginLeft: '5%',width: '90%'}} />
                 </Grid>
-
-            <Grid item sc={2} >
-                <Checkbox {...label} style={{marginBottom:'20px'}}/>
+            <Grid item xs={1} >
+                <Checkbox {...label} style={{marginBottom:'15px'}}/>
             </Grid>
             
-            <Grid item xs={10}>
+            <Grid item xs={11}>
                 <Typography align="left">     
-                    <p style={{marginRight:"30px", color:'#718291', fontFamily: 'TT Commons Thin', fontSize:"14.5px", lineHeight:'22px'}}>I agree to Money Magnet's <a href="#">Terms of Use</a> including the <a href="#">E-Sign Consent</a>, <a href="#">ACH Authorization</a> and <a href="#">Privacy Policy.</a></p>
+                    <p style={{marginRight:"22px", color:'#718291', fontFamily: 'TT Commons Thin', fontSize:"14.5px", lineHeight:'18px'}}>I agree to Money Magnet's <a href="#">Terms of Use</a> including the <a href="#">E-Sign Consent</a>, <a href="#">ACH Authorization</a> and <a href="#">Privacy Policy.</a></p>
                 </Typography>
             </Grid>
                
-
                 
         </Grid>
     )
--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -1,29 +1,31 @@
 import React from 'react';
 import MainImg from "../assets/MainImg.svg"
 import {Stack, Button } from '@mui/material'
-import styles from './styles/SignIn.css'
-
+import style from './styles/SignIn.css'
+import PhoneForm from './PhoneForm'
+import {Link} from "react-router-dom"
 export default function SignIn() {
 
 
 
     return (
-            <div className={styles.headergrid}>
-                <div item xs={10}>       
+            <div className={style.headergrid}>
+                <div>       
                     <img style={{marginTop: '-5px', width:'100%', display: 'block' }} src={MainImg} alt="MainImg" />
                 </div> 
                 <div>
                     <h1 className="hugeHeadline">Your money, your goals</h1>
                 </div>
                 <div>
-                    <p className={styles.mainParagraph}>
+                    <p>
                         Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus..
                     </p>
                 </div>
                 <div>
                     <Stack spacing={-3} direction="row">
-                        <Button style={{marginTop:'20px',width:"35%", display: "inlineflex", padding: '15px', marginLeft:"5%", backgroundColor: '#F2F5F8', marginRight:"40px"}}variant="outlined">Login</Button> 
-                        <Button style={{marginTop:'20px',width:"45%"}} variant="contained" size="small">Get Started</Button>
+                        <Button style={{marginTop:'10px',width:"35%", display: "inline-flex", padding: '15px', marginLeft:"5%", backgroundColor: '#F2F5F8', marginRight:"40px"}}variant="outlined">Login</Button> 
+                        <Button component={Link} to="/phone-form" style={{marginTop:'10px',width:"45%"}} variant="contained" size="small">Get Started</Button>
+                                            
                     </Stack>
                 </div>
             </div>
