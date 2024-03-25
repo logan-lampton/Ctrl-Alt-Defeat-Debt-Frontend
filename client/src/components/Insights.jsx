8fba024ea8a01042e55d61048df4d02763b8c713
shadman-a <ahmedshadman966@gmail.com>
2024-03-25 15:54:08 -0400

    created new insights page and added ai stuff

--- client/src/components/Insights.jsx
+++ client/src/components/Insights.jsx
@@ -1,21 +1,305 @@
-import {React, useContext} from 'react'
-import { Container } from "@mui/system";
-import { List, ListItem, Divider } from "@mui/material"
-import ArrowForward from "../assets/arrow_forward.svg"
-import './styles/Goals.css'
-import Transactions from './Transactions';
+import React, { useState, useContext, useEffect } from "react";
+import {
+  CircularProgress,
+  Container,
+  List,
+  ListItem,
+  ListItemAvatar,
+  Avatar,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import {
+  PieChart,
+  Pie,
+  Cell,
+  ResponsiveContainer,
+  Tooltip,
+  Legend,
+} from "recharts";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip as RechartsTooltip,
+  LabelList,
+} from "recharts";
+import Transactions from "./Transactions"; 
+import SpendingInsights from "./SpendingInsights";
 import { UserContext } from "../context/UserContext";
+import ArrowForward from "../assets/arrow_forward.svg";
+import InfoIcon from '@mui/icons-material/Info';
+import Sparkles from '../assets/sparkles.svg';
+import "./styles/Goals.css";
+
+
+const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
 
 export default function Insights() {
+  const { accessToken } = useContext(UserContext)
+  const [showTransactions, setShowTransactions] = useState(false);
+  const [transactions, setTransactions] = useState([]);
+  const [chartData, setChartData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [predictions, setPredictions] = useState({})
 
-  const { user, setUser} = useContext(UserContext)
+
+  const totalEarned = transactions
+    .filter((t) => t.amount > 0)
+    .reduce((acc, t) => acc + t.amount, 0);
+  const totalSpent = transactions
+    .filter((t) => t.amount < 0)
+    .reduce((acc, t) => acc + t.amount, 0);
+  // const barChartData = [
+  //     { name: 'Earned', value: totalEarned },
+  //     { name: 'Spent', value: Math.abs(totalSpent) } // Assuming spent amounts are negative
+  // ];
+
+  const spendingCategories = [
+    { id: 1, name: 'Groceries', emoji: '🍏', amount: 300 },
+    { id: 2, name: 'Transport', emoji: '🚗', amount: 150 },
+    { id: 3, name: 'Entertainment', emoji: '🎬', amount: 200 },
+    // ...other categories
+  ];
+
+  const fetchTransactions = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch("/plaid/get_transactions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ access_token: accessToken }),
+      });
+      if (!response.ok) throw new Error("Network response was not ok");
+      const data = await response.json();
+      setTransactions(data.transactions || []);
+      prepareChartData(data.transactions);
+    } catch (error) {
+      console.error("Error fetching transactions:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+ 
+  const callAI = async () => {
+    if (!accessToken) {
+      return {error: "No access token found"};
+    }
+    try {
+      const response = await fetch("/openai/response", {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          access_token: accessToken,
+        }),
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const data = await response.json();
   
-  const accessToken = user._access_token
+      // Add a delay before setting the state
+      setTimeout(() => {
+        setPredictions(data.predictions);
+      }, 1000); 
+  
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
 
-    return (
-        <Container style={{width:"430px", height:"932px", flexGrow: "0", padding:"0 0 8px"}}>
+  const prepareChartData = (transactions) => {
+    const categoryTotals = transactions.reduce((acc, transaction) => {
+      const category = transaction.category[0];
+      acc[category] = (acc[category] || 0) + transaction.amount;
+      return acc;
+    }, {});
+    const data = Object.keys(categoryTotals).map((category) => ({
+      name: category,
+      value: categoryTotals[category],
+    }));
+    setChartData(data);
+  };
 
-                <Transactions accessToken={accessToken} />
-        </Container>
-    )
+  const calculateCashFlow = (transactions) => {
+    let earned = 0;
+    let spent = 0;
+
+    transactions.forEach((transaction) => {
+      // console.log(
+      //   `Processing transaction: ${transaction.name}, Amount: ${transaction.amount}, Category: ${transaction.category}`
+      // );
+
+      // Assuming 'Transfer' and 'Credit Card Payment' are specific categories that can be used to filter out non-spending transactions
+      if (
+        !transaction.category.includes("Transfer") &&
+        !transaction.category.includes("Credit Card Payment")
+      ) {
+        if (transaction.amount > 0) {
+          earned += transaction.amount;
+        } else {
+          spent += Math.abs(transaction.amount); // Assuming spent amounts are negative, convert them to positive
+        }
+      } else if (
+        transaction.category.includes("Credit Card Payment") &&
+        transaction.amount < 0
+      ) {
+        // If the transaction is a 'Credit Card Payment' and the amount is negative,
+        // it could mean a payment was made (which should not count as earning or spending)
+        // so we ignore it or handle it accordingly.
+        console.log("Ignoring credit card payment or similar transaction.");
+      }
+    });
+
+    return { earned, spent };
+  };
+
+  const { earned, spent } = calculateCashFlow(transactions);
+
+  const barChartData = [
+    { name: "Earned", value: earned },
+    { name: "Spent", value: spent },
+  ];
+
+  useEffect(() => {
+    fetchTransactions();
+    callAI();
+  }, [accessToken]);
+
+  if (isLoading) return <CircularProgress />;
+
+  // Sort transactions by date descending to get the most recent ones
+  const recentTransactions = transactions
+    .sort((a, b) => new Date(b.date) - new Date(a.date))
+    .slice(0, 5);
+
+  return (
+    <Container
+      sx={{
+        height: "932px",
+        flexGrow: "0",
+        padding: "16px",
+        overflow: "auto",
+        paddingBottom: 2,
+      }}
+    >
+      {!showTransactions ? (
+        <>
+          <Typography variant="h6" style={{ marginTop: "20px" }}>
+            Spending Breakdown
+          </Typography>
+          <ResponsiveContainer
+            width="100%"
+            height={300}
+            style={{
+              border: "1px solid #ccc",
+              borderRadius: "5px",
+              paddingBottom: "20px",
+            }}
+          >
+            <div
+              className="goal-arrow-container"
+              onClick={() => setShowTransactions(true)}
+            >
+              <img
+                src={ArrowForward}
+                style={{ marginTop: "5px" }}
+                alt="ArrowForward"
+              />
+            </div>
+            <PieChart>
+              <Pie
+                dataKey="value"
+                isAnimationActive={false}
+                data={chartData}
+                cx="50%"
+                cy="50%"
+                outerRadius={100}
+                fill="#8884d8"
+                label={({ name, percent }) =>
+                  `${name}: ${(percent * 100).toFixed(0)}%`
+                }
+                onClick={() => setShowTransactions(true)}
+              >
+                {chartData.map((entry, index) => (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={COLORS[index % COLORS.length]}
+                  />
+                ))}
+              </Pie>
+              <Tooltip />
+              <Legend />
+            </PieChart>
+          </ResponsiveContainer>
+          <>
+            <Typography variant="h6" style={{ marginTop: "20px" }}>
+              March Cash Flow
+            </Typography>
+            <ResponsiveContainer
+              width="100%"
+              height={250}
+              style={{ border: "1px solid #ccc", borderRadius: "5px" }}
+            >
+              <BarChart
+                data={barChartData}
+                margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+              >
+                <XAxis dataKey="name" axisLine={false} tickLine={false} />
+                <YAxis hide={true} />
+                <RechartsTooltip />
+                <Bar dataKey="value" fill="#8884d8" barSize={130}>
+                  <LabelList
+                    dataKey="value"
+                    position="top"
+                    style={{ fill: "#555" }}
+                  />
+                </Bar>
+              </BarChart>
+            </ResponsiveContainer>
+          </>
+          <Typography variant="h6" gutterBottom component="div" style={{ marginTop: "20px"}}>
+        <img src={Sparkles} alt="Sparkles" style={{ verticalAlign: 'middle', marginRight: '8px' }}/>
+        AI-Generated Spending Forecasts
+        <Tooltip title="These numbers are AI-generated forecasts and are indicative. Actual figures may vary.">
+          <InfoIcon fontSize="small" style={{ verticalAlign: 'middle', marginLeft: '8px' }} />
+        </Tooltip>
+      </Typography>
+          <SpendingInsights spendingCategories={spendingCategories} predictions={predictions}/>
+          <Typography variant="h6" style={{ marginTop: "20px" }}>
+            Recent Transactions
+          </Typography>
+          <List>
+            {recentTransactions.map((transaction, index) => (
+              <ListItem key={index} divider>
+                <ListItemAvatar>
+                  {transaction.logo_url ? (
+                    <Avatar alt="Merchant Logo" src={transaction.logo_url} />
+                  ) : (
+                    <Avatar>{transaction.name[0]}</Avatar>
+                  )}
+                </ListItemAvatar>
+                <ListItemText
+                  primary={transaction.name}
+                  secondary={`Date: ${
+                    transaction.date
+                  } - Amount: $${transaction.amount.toFixed(2)}`}
+                />
+              </ListItem>
+            ))}
+          </List>
+        </>
+      ) : (
+        <Transactions
+          transactions={transactions}
+          setShowTransactions={setShowTransactions}
+        />
+      )}
+    </Container>
+  );
 }
--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState } from 'react';
 import {
   List,
   ListItem,
@@ -6,174 +6,67 @@ import {
   ListItemText,
   Avatar,
   Typography,
-  CircularProgress,
   Paper,
   Divider,
   Select,
   MenuItem,
   FormControl,
-  InputLabel
+  InputLabel,
+  Button
 } from '@mui/material';
-import { PieChart, Pie, Cell, ResponsiveContainer, Tooltip, Legend } from 'recharts';
-import { UserContext } from "../context/UserContext";
 
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#AF19FF'];
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042']; // Re-use COLORS for consistency
 
-const Transactions = ({ accessToken }) => {
-  const [transactions, setTransactions] = useState([]);
-  const [filteredTransactions, setFilteredTransactions] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState('');
-  const [totalSpent, setTotalSpent] = useState(0);
-  const [selectedCategory, setSelectedCategory] = useState('All');
-  const [categories, setCategories] = useState(['All']);
+const Transactions = ({ transactions, setShowTransactions }) => {
+    const [selectedCategory, setSelectedCategory] = useState('All');
+    const uniqueCategories = ['All', ...new Set(transactions.map(t => t.category[0]))];
+    const filteredTransactions = selectedCategory === 'All' ? transactions : transactions.filter(t => t.category[0] === selectedCategory);
 
-  const fetchTransactions = async () => {
-    setIsLoading(true);
-    setError('');
-
-    try {
-      const response = await fetch('/plaid/get_transactions', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ access_token: accessToken }),
-      });
-
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-
-      const data = await response.json();
-      
-      if (data.transactions) {
-        const transactionsWithCategories = data.transactions.map(t => ({
-          ...t,
-          category: t.category[0] // Assuming first category is the most relevant
-        }));
-        const uniqueCategories = ['All', ...new Set(transactionsWithCategories.map(t => t.category))];
-        setCategories(uniqueCategories);
-        setTransactions(transactionsWithCategories);
-        setFilteredTransactions(transactionsWithCategories);
-        calculateTotalAndChartData(transactionsWithCategories);
-      } else {
-        setError('No transactions data found');
-        setTransactions([]);
-        setFilteredTransactions([]);
-      }
-    } catch (error) {
-      setError('Failed to fetch transactions');
-      console.error('Error fetching transactions:', error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  const calculateTotalAndChartData = (transactions) => {
-    let total = 0;
-    const categoryTotals = transactions.reduce((acc, transaction) => {
-      const value = acc[transaction.category] || 0;
-      acc[transaction.category] = value + transaction.amount;
-      total += transaction.amount;
-      return acc;
-    }, {});
-    
-    const chartData = Object.keys(categoryTotals).map(category => ({
-      name: category,
-      value: categoryTotals[category]
-    }));
-    
-    setTotalSpent(total);
-    setData(chartData);
-  };
-
-  useEffect(() => {
-    fetchTransactions();
-  }, []);
-
-  useEffect(() => {
-    if (selectedCategory === 'All') {
-      setFilteredTransactions(transactions);
-    } else {
-      setFilteredTransactions(transactions.filter(t => t.category === selectedCategory));
-    }
-  }, [selectedCategory, transactions]);
-
-  const handleCategoryChange = (event) => {
-    setSelectedCategory(event.target.value);
-  };
-
-  // Dummy data for pie chart, replace with dynamic data
-  const [data, setData] = useState([]);
-
-  return (
-    <div style={{overflow: "auto"}}>
-      <Paper elevation={3} style={{ padding: '20px', marginTop: '20px' }}>
-        <Typography variant="h6">Your transaction history broken down</Typography>
-        <Typography variant="subtitle1">You have spent ${totalSpent.toFixed(2)} this month.</Typography>
-        
-        <FormControl fullWidth style={{ marginBottom: '20px' }}>
-          <InputLabel>Category</InputLabel>
-          <Select value={selectedCategory} label="Category" onChange={handleCategoryChange}>
-            {categories.map((category, index) => (
-              <MenuItem key={index} value={category}>{category}</MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        <ResponsiveContainer width="100%" height={300}>
-          <PieChart>
-            <Pie
-              dataKey="value"
-              isAnimationActive={false}
-              data={data}
-              cx="50%"
-              cy="50%"
-              outerRadius={80}
-              fill="#8884d8"
-              label={({ name, percent }) => `${name}: ${(percent * 100).toFixed(0)}%`}
-            >
-              {data.map((entry, index) => (
-                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+    return (
+        <Paper elevation={3} style={{ padding: '20px', marginTop: '20px' }}>
+            <Button onClick={() => setShowTransactions(false)}>Back to Insights</Button>
+            <FormControl fullWidth>
+                <InputLabel>Category</InputLabel>
+                <Select
+                    value={selectedCategory}
+                    label="Category"
+                    onChange={e => setSelectedCategory(e.target.value)}
+                >
+                    {uniqueCategories.map((category, index) => (
+                        <MenuItem key={index} value={category}>{category}</MenuItem>
+                    ))}
+                </Select>
+            </FormControl>
+            <List style={{ maxHeight: '600px', overflow: 'auto' }}>
+                {filteredTransactions.map((transaction, index) => (
+                  <React.Fragment key={index}>
+                      <ListItem alignItems="flex-start">
+                          <ListItemAvatar>
+                              {transaction.logo_url ? (
+                                  <Avatar alt="Merchant Logo" src={transaction.logo_url} />
+                              ) : (
+                                  <Avatar>{transaction.name[0]}</Avatar>
+                              )}
+                          </ListItemAvatar>
+                          <ListItemText
+                              primary={transaction.name}
+                              secondary={
+                                  <>
+                                      <Typography component="span" variant="body2" color="text.primary">
+                                          Date: {transaction.date} - Amount: ${transaction.amount.toFixed(2)}
+                                      </Typography>
+                                      <br />
+                                      Category: {transaction.category[0]}
+                                  </>
+                              }
+                          />
+                      </ListItem>
+                      {index < filteredTransactions.length - 1 && <Divider variant="inset" component="li" />}
+                  </React.Fragment>
               ))}
-            </Pie>
-            <Legend />
-            <Tooltip />
-          </PieChart>
-        </ResponsiveContainer>
-        <List style={{ maxHeight: '400px', overflow: 'auto' }}>
-  {filteredTransactions.map((transaction, index) => (
-    <React.Fragment key={index}>
-      <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          {transaction.logo_url ? (
-            <Avatar alt="Merchant Logo" src={transaction.logo_url} />
-          ) : (
-            <Avatar>{transaction.name[0]}</Avatar>
-          )}
-        </ListItemAvatar>
-        <ListItemText
-          primary={transaction.name}
-          secondary={
-            <>
-              <Typography component="span" variant="body2" color="text.primary">
-                Date: {transaction.date} - Amount: ${transaction.amount.toFixed(2)}
-              </Typography>
-              <br />
-              Category: {transaction.category}
-            </>
-          }
-        />
-      </ListItem>
-      {index < filteredTransactions.length - 1 && <Divider variant="inset" component="li" />}
-    </React.Fragment>
-  ))}
-</List>
+          </List>
       </Paper>
-    </div>
   );
 };
 
 export default Transactions;
-
-       
